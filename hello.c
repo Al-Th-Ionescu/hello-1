120f76650ea7acaeed25843fd13b14adb5e655e4
MaximTiberiu <maxim.tiberiu.info@gmail.com>
2020-04-03 11:07:07 +0300

    hello.c saluta utilizatorul introdus de la tastatura

--- hello.c
+++ hello.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+int main(int argc, char *argv[])
 {
-	printf("Hello, World!\n");
+	if(argc != 2)
+	{
+		printf("USAGE: %s name\n", argv[0]);
+		return -1;
+	}
+	printf("Hello, %s!\n", argv[1]);
 	retunr 0;
 }
