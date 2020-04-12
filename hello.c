361d84a1728e2cd43cbd96b3d10afe64f024521f
Al-Th-Ionescu <alexandru-theodor.ionescu@my.fmi.unibuc.ro>
2020-04-12 15:17:40 +0300

    Exercitiul 5

--- hello.c
+++ hello.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main(int argc, char *argv[])
 {
 	if(argc != 2)
 	{
 		printf("USAGE: %s name\n", argv[0]);
-		return -1;
+		return -5;
 	}
+	//preluat de la Tiberiu
 	printf("Hello, %s!\n", argv[1]);
-	retunr 0;
+	return 0;
 }
